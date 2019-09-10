0c43892e9649649a11e3236b25c987d0cdd14ac6
Painezeng <tozeng@vip.qq.com>
2019-09-10 13:22:07 +0800

    case study 2

--- CSharpAndSolidWorks/Form1.Designer.cs
+++ CSharpAndSolidWorks/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnConnect = new System.Windows.Forms.Button();
+            this.btnOpenAndNew = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -41,14 +42,26 @@
             this.btnConnect.UseVisualStyleBackColor = true;
             this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
             // 
+            // btnOpenAndNew
+            // 
+            this.btnOpenAndNew.Location = new System.Drawing.Point(23, 74);
+            this.btnOpenAndNew.Name = "btnOpenAndNew";
+            this.btnOpenAndNew.Size = new System.Drawing.Size(89, 30);
+            this.btnOpenAndNew.TabIndex = 1;
+            this.btnOpenAndNew.Text = "打开和创建";
+            this.btnOpenAndNew.UseVisualStyleBackColor = true;
+            this.btnOpenAndNew.Click += new System.EventHandler(this.btnOpenAndNew_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(430, 256);
+            this.Controls.Add(this.btnOpenAndNew);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
             this.Text = "Solidworks二次开发";
+            this.TopMost = true;
             this.ResumeLayout(false);
 
         }
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnOpenAndNew;
     }
 }
 
--- CSharpAndSolidWorks/Form1.cs
+++ CSharpAndSolidWorks/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,8 +27,57 @@ namespace CSharpAndSolidWorks
             if (swApp != null)
             {
                 string msg = "This message from C#. solidworks version is " + swApp.RevisionNumber();
-
+                //发一个消息给solidworks用户
                 swApp.SendMsgToUser(msg);
+            }
+        }
+
+        private void btnOpenAndNew_Click(object sender, EventArgs e)
+        {
+            ISldWorks swApp = Utility.ConnectToSolidWorks();
+
+            if (swApp != null)
+            {
+                //通过GetDocumentTemplate 获取默认模板的路径 ,第一个参数可以指定类型
+                string partDefaultTemplate = swApp.GetDocumentTemplate((int)swDocumentTypes_e.swDocPART, "", 0, 0, 0);
+                //也可以直接指定slddot asmdot drwdot
+                //partDefaultTemplate = @"xxx\..prtdot";
+
+                var newDoc = swApp.NewDocument(partDefaultTemplate, 0, 0, 0);
+
+                if (newDoc != null)
+                {
+                    //创建完成
+                    swApp.SendMsgToUser("Create done.");
+
+                    //下面获取当前文件
+                    ModelDoc2 swModel = (ModelDoc2)swApp.ActiveDoc;
+
+                    //选择对应的草图基准面
+                    bool boolstatus = swModel.Extension.SelectByID2("Plane1", "PLANE", 0, 0, 0, false, 0, null, 0);
+
+                    //创建一个2d草图
+                    swModel.SketchManager.InsertSketch(true);
+
+                    //画一条线 长度100mm  (solidworks 中系统单位是米,所以这里写0.1)
+                    swModel.SketchManager.CreateLine(0, 0, 0, 0, 0.1, 0);
+
+                    //关闭草图
+                    swModel.SketchManager.InsertSketch(true);
+
+                    string myNewPartPath = @"C:\study\myNewPart.SLDPRT";
+
+                    //保存零件.
+                    int longstatus = swModel.SaveAs3(myNewPartPath, 0, 1);
+
+                    //关闭零件
+                    swApp.CloseDoc(myNewPartPath);
+                    swApp.SendMsgToUser("Closed");
+                    //重新打开零件.
+                    swApp.OpenDoc(myNewPartPath, (int)swDocumentTypes_e.swDocPART);
+
+                    swApp.SendMsgToUser("Open completed.");
+                }
             }
         }
     }
